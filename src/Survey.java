f82a2317bbd55684b61e0c43b9275b1a37460d0a
wazwaz88 <wazwazabdullah@gmail.com>
2022-04-13 20:41:56 -0500

    finished project

--- src/Survey.java
+++ src/Survey.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Survey {
     public static void main(String[] args) {
@@ -14,6 +16,70 @@ public class Survey {
         -Print the age of the youngest person
         -Print the age of the eldest person
          */
+
+
+        //Global variables or objects needed
+        Scanner userInput = new Scanner(System.in);
+        ArrayList<Participant> participants = new ArrayList<>();
+        int youngest = Integer.MAX_VALUE;
+        int eldest = 0;
+
+
+
+
+        do {
+
+            System.out.println(SurveyQuestions.askToJoin);
+            String join = userInput.next();
+
+            if(join.toUpperCase().startsWith("Y")){
+
+                //ask your questions to get name, age, and gender
+
+                Participant.addParticipants();
+
+                System.out.println(SurveyQuestions.askName);
+                String name = userInput.next();
+
+                System.out.println(SurveyQuestions.askAge);
+                int age = userInput.nextInt();
+
+                System.out.println(SurveyQuestions.askGender);
+                String gender = userInput.next();
+
+                Participant participant = new Participant(name, age, gender);
+                participants.add(participant);
+
+
+
+
+                if(gender.toUpperCase().startsWith("M")) Participant.addMaleParticipants();
+                else Participant.addFemaleParticipants();
+
+
+            }
+
+
+
+        }while(Participant.totalNumberOfParticipants < 3);
+
+
+        for (Participant element : participants) {
+            System.out.println(element);
+            if(element.age > eldest) eldest = element.age;
+            if(element.age < youngest) youngest = element.age;
+
+        }
+
+        System.out.println("The total number of male participants = " + Participant.totalNumberOfMaleParticipants);
+        System.out.println("The total number of female participants = " + Participant.totalNumberOfFemaleParticipants);
+
+
+        System.out.println("The youngest age of the participants = " + youngest);
+        System.out.println("The oldest age of the participants = " + eldest);
+
+
+
 
     }
 }
--- src/SurveyQuestions.java
+++ src/SurveyQuestions.java
@@ -8,4 +8,13 @@ public class SurveyQuestions {
     What is your gender? (F/M) -> -> variable name can be askGender
      */
 
+
+
+    public static final String askToJoin = "Would you like to join our survey? (Y/N)";
+    public static final String askName = "What is your name?";
+    public static final String askAge = "How old are you?";
+    public static final String askGender = "What is your gender? (F/M)";
+
+
+
 }
